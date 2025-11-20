f83a5b015b732ac94cb741941e8a90a163cb1f9f
owenlee <owenlzyxg@gmail.com>
2025-11-20 15:21:16 +0800

    Initial Commit

---
+++ Moneywise/MoneywiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneywiseApp.swift
+//  Moneywise
+//
+//  Created by Owen Lee Zhao Yi on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoneywiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneywiseTests/MoneywiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneywiseTests.swift
+//  MoneywiseTests
+//
+//  Created by Owen Lee Zhao Yi on 20/11/2025.
+//
+
+import Testing
+@testable import Moneywise
+
+struct MoneywiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
